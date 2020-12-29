1591e4c0e53916064ea1cba7f9b1f917732f68b4
Nikolay Murzin <murzin.nikolay@gmail.com>
2020-12-29 17:07:17 +0300

    Improve Brace

--- WolfAnim/Primitives.m
+++ WolfAnim/Primitives.m
@@ -5,8 +5,10 @@ PackageExport["Brace"]
 
 Options[Brace] = {"Direction" -> Down, "WidthMultiplier" -> 2, "Buffer" -> 0.2}
 
-Brace[obj_AnimatedObject, dir_ : $AnimatedObjectDefaultDirective, OptionsPattern[]] := Module[{
-    direction, angle, corners, left, right, width, numQuads, text
+Brace[obj_AnimatedObject, Optional[dir : _ ? directiveQ | Automatic, $AnimatedObjectDefaultDirective],
+    Optional[text_AnimatedObject, AnimatedObject[""]],
+    opts : OptionsPattern[]] := Module[{
+    direction, angle, corners, left, right, width, numQuads, brace, tip
 },
     direction = OptionValue["Direction"] /. {Left -> {-1, 0}, Right -> {1, 0}, Down | Bottom -> {0, -1}, Up | Top -> {0, 1}};
     angle = ArcTan @@ Reverse[direction] - Pi;
@@ -14,6 +16,10 @@ Brace[obj_AnimatedObject, dir_ : $AnimatedObjectDefaultDirective, OptionsPattern
     {left, right} = Values@corners[[{Key[{-1, -1}], Key[{1, -1}]}]];
     width = First[right] - First[left];
     numQuads = Clip[width OptionValue["WidthMultiplier"], {2, 15}];
-    text = AnimatedObject[StringTemplate["\\underbrace{``}"][StringJoin @ Table["\\qquad", numQuads]]]["Apply", "Stretch", width];
-    text["Apply", "Translate", left - text["Corners"][{-1, 1}] + {0, -OptionValue["Buffer"]}]["TransformPrimitives", RotationTransform[-angle]]
+    brace = AnimatedObject[StringTemplate["\\underbrace{``}"][StringJoin @ Table["\\qquad", numQuads]], dir /. Automatic -> obj["Directive"]]["Apply", "Stretch", width];
+    tip = RotationTransform[- angle][left + {width / 2, -  (OptionValue["Buffer"] + 3 brace["Height"])}];
+    AnimatedObject[{
+        text["Apply", "Translate", tip],
+        brace["Apply", "Translate", left - brace["Corners"][{-1, 1}] + {0, - OptionValue["Buffer"]}]["TransformPrimitives", RotationTransform[- angle]]
+    }]
 ]
